b15d1f6a5b220e93a0dfe13a1b03750322329833
benjp009 <patin.benjamin@gmail.com>
2021-12-20 22:09:56 +0100

    Html and css for the footer

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 import './style.css';
+import '@fortawesome/fontawesome-free/js/fontawesome'
+import '@fortawesome/fontawesome-free/js/solid'
+import '@fortawesome/fontawesome-free/js/regular'
+import '@fortawesome/fontawesome-free/js/brands'
 
 // eslint-disable-next-line no-unused-vars
 import html from './index.html';
