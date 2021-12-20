67dc9c5ca3be50a90f19200a9a7dc0df8a232fbc
benjp009 <patin.benjamin@gmail.com>
2021-12-20 22:20:27 +0100

    linters

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import './style.css';
-import '@fortawesome/fontawesome-free/js/fontawesome'
-import '@fortawesome/fontawesome-free/js/solid'
-import '@fortawesome/fontawesome-free/js/regular'
-import '@fortawesome/fontawesome-free/js/brands'
+import '@fortawesome/fontawesome-free/js/fontawesome.js';
+import '@fortawesome/fontawesome-free/js/solid.js';
+import '@fortawesome/fontawesome-free/js/regular.js';
+import '@fortawesome/fontawesome-free/js/brands.js';
 
 // eslint-disable-next-line no-unused-vars
 import html from './index.html';
