771545379713f96637d7a49c915996157504624c
koomekevo <koome.kevo@gurucrafts.net>
2021-12-21 10:00:55 +0300

    Modify index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import './style.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import populateList from './homepage.js';
 
 // eslint-disable-next-line no-unused-vars
 import html from './index.html';
