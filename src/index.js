994594cff3e039d9c52b46229df15aa34c45bd2f
benjp009 <patin.benjamin@gmail.com>
2021-12-21 09:34:42 +0100

    Change logo

--- src/index.js
+++ src/index.js
@@ -1,8 +1,4 @@
 import './style.css';
-import '@fortawesome/fontawesome-free/js/fontawesome.js';
-import '@fortawesome/fontawesome-free/js/solid.js';
-import '@fortawesome/fontawesome-free/js/regular.js';
-import '@fortawesome/fontawesome-free/js/brands.js';
 
 // eslint-disable-next-line no-unused-vars
 import html from './index.html';
