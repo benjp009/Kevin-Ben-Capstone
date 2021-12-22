2d902bdf43c6a9273d62888f316d48680c72370c
benjp009 <patin.benjamin@gmail.com>
2021-12-22 11:59:01 +0100

    removing fetch data

--- src/fetchdata.js
+++
@@ -1,8 +0,0 @@
-const fetchDataApi = () => {
-
-  fetch('https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood')
-    .then(response => response.json())
-    .then(data => console.log(data));
-};
-
-export default fetchDataApi;
