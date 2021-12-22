ca7f21962ec887177762ae7f3d6e926532c89d1d
benjp009 <patin.benjamin@gmail.com>
2021-12-22 12:02:41 +0100

    Update homepage.js

--- src/homepage.js
+++ src/homepage.js
@@ -1,16 +1,23 @@
 import createList from './homePageRequest.js';
 
+const listContainer = document.getElementById('homepageList');
+
 const populateList = async (category) => {
   const itemList = await createList(category);
   for (let i = 0; i < itemList.length; i += 1) {
-    document.getElementById('homepageList').innerHTML += `
-    <div class='fav-meal'>
-      <img class='meal-image' src='${itemList[i].strMealThumb}' alt=''>
-      <h2 class='meal-title'>${itemList[i].strMeal}</h2>
-      <button class='meal-button' type='button' name='Comments'>Comments</button>
-      <button class='meal-button' type='button' name='Reservation'>Reservation</button>
-    </div>`;
+    if (itemList[i].strMealThumb !== undefined && itemList[i].strMealThumb !== null) {
+      document.getElementById('homepageList').innerHTML += `
+      <div class='fav-meal'>
+        <img class='meal-image' src='${itemList[i].strMealThumb}' alt=''>
+        <h2 class='meal-title'>${itemList[i].strMeal}</h2>
+        <button class='meal-button' type='button' name='Comments'>Comments</button>
+        <button class='meal-button' type='button' name='Reservation'>Reservation</button>
+      </div>`;
+    }
   }
-}
+  footer.classList.remove('d-none');
+  footer.classList.add('d-md-flex');
+  addLikes();
+};
 
 export default populateList;
