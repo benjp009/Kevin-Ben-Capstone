6b9d3dfde6ebeca50aebebcb3f76acf177c051f6
benjp009 <patin.benjamin@gmail.com>
2021-12-22 11:26:25 +0100

    Add Homepage Request js

---
+++ src/homePageRequest.js
@@ -0,0 +1,23 @@
+const getCategory = async (category) => {
+    const url = `https://www.themealdb.com/api/json/v1/1/filter.php?c=${category}`;
+    const response = await fetch(url);
+    const parsed = await response.json();
+    return parsed.meals;
+  };
+
+  const createList = async (category) => {
+    const list = [];
+    const result = await getCategory(category);
+    if (result.length > 12) {
+      for (let i = 0; i < 12; i += 1) {
+        list.push(result[i]);
+      }
+    } else {
+      for (let i = 0; i < result.length; i += 1) {
+        list.push(result[i]);
+      }
+    }
+    return list;
+  };
+
+  export default createList;
