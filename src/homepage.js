b8ab8dca18e6c86da3f151c5457901497ab1cc11
benjp009 <patin.benjamin@gmail.com>
2021-12-22 13:59:21 +0100

    Add likes to the framework of the page

--- src/homepage.js
+++ src/homepage.js
@@ -1,6 +1,18 @@
 import createList from './homePageRequest.js';
 
 // const listContainer = document.getElementById('homepageList');
+const APIUrl = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/J8Ya3HGGvBBaT8zGxBGx/likes';
+
+const getLikes = async (id) => {
+  const likesList = await fetch(APIUrl);
+  const likesObject = await likesList.json();
+  for (let j = 0; j < likesObject.length; j += 1) {
+    if (likesObject[j].item_id === Number(id)) {
+      return likesObject[j].likes;
+    }
+  }
+  return 0;
+};
 
 const populateList = async (category) => {
   const itemList = await createList(category);
@@ -9,7 +21,13 @@ const populateList = async (category) => {
       document.getElementById('homepageList').innerHTML += `
       <div class='fav-meal'>
         <img class='meal-image' src='${itemList[i].strMealThumb}' alt=''>
-        <h2 class='meal-title'>${itemList[i].strMeal}</h2>
+        <div>
+          <h2 class='meal-title'>${itemList[i].strMeal}</h2>
+          <div class='meal-likes'>
+            <p class='like-items'>likes<p>
+            <p class='like-items'><i class='fas fa-heart'></i></p>
+          </div>
+        </div>
         <button class='meal-button' type='button' name='Comments'>Comments</button>
         <button class='meal-button' type='button' name='Reservation'>Reservation</button>
       </div>`;
@@ -21,3 +39,6 @@ const populateList = async (category) => {
 };
 
 export default populateList;
+
+
+// ${likesCount} likes <a class='likeBtn' id='${itemList[i].idMeal}'><i class='far fa-grin-hearts fs-4 like-icon transition-all'
