95d9747587b583a72ab85ba876ea19b1b1250c72
benjp009 <patin.benjamin@gmail.com>
2021-12-22 12:02:54 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,41 @@
 import './style.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 // eslint-disable-next-line no-unused-vars
 import html from './index.html';
 
 // eslint-disable-next-line no-unused-vars
-import itemList from './homepage.js';
+import populateList from './homepage.js';
+
+const categories = document.querySelectorAll('.navCategories');
+const listContainer = document.getElementById('homepageList');
+
+const commentBtnEvent = async (category) => {
+  await populateList(category);
+  const commentBtns = document.querySelectorAll('.commentBtn');
+  commentBtns.forEach((e) => {
+    e.addEventListener('click', () => {
+      createModal(e.id);
+    });
+  });
+};
+
+categories.forEach((e) => {
+  e.addEventListener('click', async () => {
+    listContainer.innerHTML = '';
+    categories.forEach((f) => {
+      if (f.id !== e.id) {
+        f.innerHTML = f.id;
+      }
+    });
+    await commentBtnEvent(e.id);
+    displayCount(e, listContainer.children.length);
+  });
+});
+
+const firstPopulate = async () => {
+  await commentBtnEvent('Dessert');
+  displayCount(categories[0], listContainer.children.length);
+};
+
+firstPopulate();
