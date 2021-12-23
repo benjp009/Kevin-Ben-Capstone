c0a309410606681b915da744462c1c11ad61a14b
koomekevo <koome.kevo@gurucrafts.net>
2021-12-24 02:31:28 +0300

    Add comments popup

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import './style.css';
+import './queries.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import createModal from './modalCreation.js';
 import populateList from './homepage.js';
