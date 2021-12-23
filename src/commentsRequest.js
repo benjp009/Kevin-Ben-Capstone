60987a93a8fd5f3f88d4c996c56cc6bcce1e9e23
koomekevo <koome.kevo@gurucrafts.net>
2021-12-23 16:12:09 +0300

    Add comments-counter

--- src/commentsRequest.js
+++ src/commentsRequest.js
@@ -1,4 +1,3 @@
-// eslint-disable-next-line no-unused-vars
 const getItem = async (id) => {
   const url = (`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`);
   const response = await fetch(url);
@@ -6,10 +5,24 @@ const getItem = async (id) => {
   return parsed.meals[0];
 };
 
-// eslint-disable-next-line no-unused-vars
 const getComments = async (id) => {
   const comments = `https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/J8Ya3HGGvBBaT8zGxBGx/comments?item_id=${id}`;
   const response = await fetch(comments);
   const result = await response.json();
   return result;
 };
+
+const commentCounter = async (id) => {
+  const commentList = await getComments(id);
+  if (commentList.length === undefined) {
+    return 0;
+  }
+
+  if (commentList === Error) {
+    return 0;
+  }
+
+  return commentList.length;
+};
+
+export { getItem as default, getComments, commentCounter };
