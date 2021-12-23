2b0374f242bbc5339db46600b777762bed3ef99b
benjp009 <patin.benjamin@gmail.com>
2021-12-23 13:23:05 +0100

    create test file

---
+++ src/index.test.js
@@ -0,0 +1,7 @@
+const firstPopulate = require('./index.js');
+
+it('display itemscount on categories', () => {
+  expect.assertions(1);
+  const firstPopulate = await commentBtnEvent(12);
+  expect(e).toEqual(12);
+})
