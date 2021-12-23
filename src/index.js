744e960c1bc59ccfb312b34f8894051e416be5a6
koomekevo <koome.kevo@gurucrafts.net>
2021-12-23 13:33:11 +0300

    Add modal

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 import './style.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-// eslint-disable-next-line no-unused-vars
-import html from './index.html';
-
-// eslint-disable-next-line no-unused-vars
+import createModal from './modalCreation.js';
 import populateList from './homepage.js';
+import displayCount from './itemscount.js';
 
 const categories = document.querySelectorAll('.navCategories');
 const listContainer = document.getElementById('homepageList');
@@ -15,7 +12,7 @@ const commentBtnEvent = async (category) => {
   const commentBtns = document.querySelectorAll('.commentBtn');
   commentBtns.forEach((e) => {
     e.addEventListener('click', () => {
-      // createModal(e.id);
+      createModal(e.id);
     });
   });
 };
@@ -29,13 +26,13 @@ categories.forEach((e) => {
       }
     });
     await commentBtnEvent(e.id);
-    // displayCount(e, listContainer.children.length);
+    displayCount(e, listContainer.children.length);
   });
 });
 
 const firstPopulate = async () => {
   await commentBtnEvent('Dessert');
-  // displayCount(categories[0], listContainer.children.length);
+  displayCount(categories[0], listContainer.children.length);
 };
 
 firstPopulate();
