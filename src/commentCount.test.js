c53c5ed782c9a89034a14a71c445bea7b57247bb
koomekevo <koome.kevo@gurucrafts.net>
2021-12-24 03:46:27 +0300

    Add test student B

---
+++ src/commentCount.test.js
@@ -0,0 +1,58 @@
+import { commentCounter } from './commentsRequest.js';
+
+const unmockedFetch = global.fetch;
+
+beforeAll(() => {
+  global.fetch = () => Promise.resolve({
+    json: () => Promise.resolve([
+      {
+        comment: 'I did it like three times',
+        creation_date: '2021-08-03',
+        username: 'Kevin',
+      },
+    ]),
+  });
+});
+
+afterAll(() => {
+  global.fetch = unmockedFetch;
+});
+
+test('It counts the right number of comments1', async () => {
+  const commentNumber = await commentCounter(52772);
+  expect(commentNumber).toEqual(1);
+});
+
+test('It counts the right number of comments1', async () => {
+  global.fetch = () => Promise.resolve({
+    json: () => Promise.resolve([
+      {
+        comment: 'I did it like three times',
+        creation_date: '2021-08-03',
+        username: 'Kevin',
+      },
+      {
+        comment: 'I did it like three times',
+        creation_date: '2021-08-03',
+        username: 'Kevin',
+      },
+      {
+        comment: 'I did it like three times',
+        creation_date: '2021-08-03',
+        username: 'Kevin',
+      },
+    ]),
+  });
+
+  const commentNumber = await commentCounter(52772);
+  expect(commentNumber).toEqual(3);
+});
+
+test('It counts the right number of comments2', async () => {
+  global.fetch = () => Promise.resolve({
+    json: () => Promise.resolve(Error),
+  });
+
+  const commentNumber = await commentCounter(52772);
+  expect(commentNumber).toBe(0);
+});
